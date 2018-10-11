0a78fbaabc009d3b5e31f0e0701f94258f5e64ec
Alexander Vincent-Hill <alexanderjvincenthill@gmail.com>
2018-10-11 11:26:08 -0700

    Add start-prod script

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ app.use(morgan('dev'));
 app.use('/songs/:id', express.static('public'));
 
 app.get('/api/waveformplayer/:id', (req, res) => {
-  console.log("proxy server detected a get request to /api/waveformplayer/:id");
-  axios(`http://localhost:1337/api/waveformplayer/${req.params.id}`)
+  console.log("proxy server sent a GET request to /api/waveformplayer/:id");
+  axios(`http://sdclb-2122289528.us-west-1.elb.amazonaws.com/api/waveformplayer/${req.params.id}`)
     .then(function (response) {
       res.send(response.data)
     })
@@ -26,51 +26,51 @@ app.get('/api/waveformplayer/:id', (req, res) => {
     })
 });
 
-app.get('/relatedTracks/:id', (req, res) => {
-  // res.send(req.params.id)
-  const songId = req.params.id;
-  axios.get(`http://18.219.127.175/relatedTracks/${songId}`)
-    .then(({ data }) => {
-      console.log('Related Tracks', data);
-      res.json(data);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-});
+// app.get('/relatedTracks/:id', (req, res) => {
+//   // res.send(req.params.id)
+//   const songId = req.params.id;
+//   axios.get(`http://18.219.127.175/relatedTracks/${songId}`)
+//     .then(({ data }) => {
+//       console.log('Related Tracks', data);
+//       res.json(data);
+//     })
+//     .catch((error) => {
+//       console.log(error);
+//     });
+// });
 
-app.get('/relatedAlbums/:id', (req, res) => {
-  const songId = req.params.id;
-  axios.get(`http://18.219.127.175/relatedAlbums/${songId}`)
-    .then(({ data }) => {
-      console.log('Related Albums', data);
-      res.json(data);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-});
+// app.get('/relatedAlbums/:id', (req, res) => {
+//   const songId = req.params.id;
+//   axios.get(`http://18.219.127.175/relatedAlbums/${songId}`)
+//     .then(({ data }) => {
+//       console.log('Related Albums', data);
+//       res.json(data);
+//     })
+//     .catch((error) => {
+//       console.log(error);
+//     });
+// });
 
-app.get('/api/stats/:id', (req, res) => {
-  axios.get(`http://server-env-1.phjpybupp3.us-west-1.elasticbeanstalk.com/api/stats/${req.params.id}`)
-    .then(({ data }) => {
-      res.json(data);
-    })
-    .catch((error) => {
-      console.log(error);
-    })
-})
+// app.get('/api/stats/:id', (req, res) => {
+//   axios.get(`http://server-env-1.phjpybupp3.us-west-1.elasticbeanstalk.com/api/stats/${req.params.id}`)
+//     .then(({ data }) => {
+//       res.json(data);
+//     })
+//     .catch((error) => {
+//       console.log(error);
+//     })
+// })
 
-app.get('/api/comments/:id', (req, res) => {
-  // console.log(`http:/localhost:3001/api/${req.params.songid}`);
-  axios.get(`http://comments-server.2u82f9p8mx.us-east-2.elasticbeanstalk.com/api/comments/${req.params.id}`)
-    .then(function (response) {
-      res.send(response.data);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-});
+// app.get('/api/comments/:id', (req, res) => {
+//   // console.log(`http:/localhost:3001/api/${req.params.songid}`);
+//   axios.get(`http://comments-server.2u82f9p8mx.us-east-2.elasticbeanstalk.com/api/comments/${req.params.id}`)
+//     .then(function (response) {
+//       res.send(response.data);
+//     })
+//     .catch(function (error) {
+//       console.log(error);
+//     });
+// });
 
 app.listen(port, () => {
   console.log(`avincenthill proxy server listening at ${port}...`);
